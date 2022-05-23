a7425422f467cbd66e63860f4e388f7bacb34578
Shannon Goddard <Uthingkithunk@aol.com>
2022-05-23 16:32:26 -0700

    styling nav bar

--- static/js/app.js
+++ static/js/app.js
@@ -60,8 +60,8 @@ $("li").on("click" , "a" , function(){
     document.getElementById("myUL").style.display = "none";
   }
 });
-// Toggle between adding and removing the "responsive" class to the navbar when the user clicks on the icon// 
-function navFunction() {
+// Toggle between adding and removing the "responsive" class to the navbar when the user clicks on the icon //
+function myFunction() {
   var x = document.getElementById("myNavbar");
   if (x.className === "navbar") {
     x.className += " responsive";
