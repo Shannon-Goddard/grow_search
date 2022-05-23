0b180b277803544ef8ade4803b1744e59894d1d6
Shannon Goddard <Uthingkithunk@aol.com>
2022-05-23 15:03:32 -0700

    adding nav-bar

--- static/js/app.js
+++ static/js/app.js
@@ -60,3 +60,12 @@ $("li").on("click" , "a" , function(){
     document.getElementById("myUL").style.display = "none";
   }
 });
+// Toggle between adding and removing the "responsive" class to the navbar when the user clicks on the icon// 
+function navFunction() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
